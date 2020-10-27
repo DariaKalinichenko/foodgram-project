7689f1b07fa7e7b1d1481f25711525f2e43500dd
DariaKalinichenko <kddm2015@gmail.com>
2020-10-27 22:18:13 +0300

    Edited settings, urls

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -32,9 +32,3 @@ urlpatterns = [
 
 handler404 = "recipe.views.page_not_found"  # noqa
 handler500 = "recipe.views.server_error"  # noqa
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)

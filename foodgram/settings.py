27f2a762fdfdfd3b97b9d1e968401c6fa667da6b
DariaKalinichenko <kddm2015@gmail.com>
2020-10-28 09:12:03 +0300

    Edited docker

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -82,16 +82,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'foodgramdb',
-#         'USER': 'admin',
-#         'PASSWORD': 'admin',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
